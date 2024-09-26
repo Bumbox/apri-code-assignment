56edb3b83239093c4ee23277c5839d2aaa36cb33
Sem <xbumbox@mail.ru>
2024-09-26 20:23:32 +0500

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="apri-code-assignment" />
+    </div>
+  );
+}
+
+export default App;
